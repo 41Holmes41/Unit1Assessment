81eaf8f49ef7fb030dc0ad7951bd9084fe0ca4ff
Mitchell Coleman Holmes <51342931+41Holmes41@users.noreply.github.com>
2019-08-09 10:20:50 -0500

    adds all files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let add = document.querySelector('.add');
+let minus = document.querySelector('.minus');
+let input = document.querySelector('input');
+let counter = document.querySelector('.counter');
+let total = 0;
+
+add.addEventListener('click', function(){
+    total += parseInt(input.value);
+    render();
+})
+
+minus.addEventListener('click', function(){
+    total-=parseInt(input.value);
+    render();
+})
+
+function render() {
+    counter.innerHTML=total;
+
+}
